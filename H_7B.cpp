91a77e332d01410918bbc98918163bd17f52cc62
brianmle86 <brianmle862@gmail.com>
2018-11-29 19:28:50 -0800

    hw7 done

--- H_7B.cpp
+++ H_7B.cpp
@@ -1,19 +1,209 @@
 /*
   Exam Statistics.
  
-  NAME
+NAME: Brian Le
 */
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+
 using namespace std;
 
 // Function prototypes
+void print_info(void);
+bool get_scores(ifstream &, string &, int &);
+void get_arrays(string [], int [], ifstream &, string &, int &);
+double get_avg(int [], int);
+int get_highest(int [], int);
+int get_lowest(int [], int);
+void display_results(string [], int [], int, int, int, double); 
+void write_outfile(ofstream &, string [], int [], int, double); 
+void farewell(void);
 
+int main() {
+    const int SIZE = 45;
+    int size, score, num_students = 0, max, min;
+    ifstream inFile;
+    ofstream outFile;
+    string id, filename;
+    string student_ids[SIZE];
+    int exam_scores[SIZE];
+    double avg;
 
-int main()
-{
+    print_info();
 
-
-   return 0;
+    inFile.open("scores.txt");
+    if (!inFile) {
+        cout << "Could not open file.\n";
+        return 1;
+    }
+    while(get_scores(inFile, id, score)) {
+        num_students++;
+        get_arrays(student_ids, exam_scores, inFile, id, score);
+    }
+    inFile.close();
+    avg = get_avg(exam_scores, num_students);
+    max = get_highest(exam_scores, num_students);
+    min = get_lowest(exam_scores, num_students);
+    display_results(student_ids, exam_scores, max, min, num_students, avg);   
+    
+    cout << "\nPlease enter a file name for the output file: ";
+    cin >> filename;
+    outFile.open(filename);
+    if (!outFile) {
+        cout << "Could not open file.\n";
+        return 1;
+    }
+    write_outfile(outFile, student_ids, exam_scores, num_students, avg);
+    outFile.close();
+    farewell();
+    return 0;
 }
+/**
+ This function displays basic information about
+ the program.
+ */
+void print_info() {
+    cout << "This program reads scores.txt, which contains student IDs with their\n"
+         << "respective test scores, sorts them into two arrays, and displays:\n"
+         << "-The total number of students\n"
+         << "-The class average\n"
+         << "-lowest score with that student's ID\n"
+         << "-highest score with that student's ID\n"
+         << "The student IDs of those who scored less than the average will\n"
+         << "be stored in an output file with a user-specified name.\n\n";
+}
+
+/**
+ This function returns true while it can read a string
+ and an integer from a file, and returns false otherwise.
+ */
+bool get_scores(ifstream &inFile, string &id, int &score) {
+    return bool(inFile >> id >> score);
+}
+
+/**
+ This function takes two arrays, an input file, id,
+ and score as arguments. The fuction stores the id and score
+ in the first indexes of the two arrays, and every time the
+ function is called, it stores the next ids and scores in the
+ following indexes. The result is two parallel arrays with
+ corresponding student IDs and scores.
+ */
+void get_arrays(string student_ids[], int exam_scores[],
+                ifstream &inFile, string &id, int &score) {
+    static int i = 0;
+    student_ids[i] = id;
+    exam_scores[i] = score;
+    i++;
+}
+/**
+ This function takes an array of scores and the number
+ of students as arguments. It returns the average score.
+ */ 
+double get_avg(int exam_scores[], int num_students) {
+    double total = 0, avg;
+    for(int i = 0; i < num_students; i++) {
+        total += exam_scores[i];
+    }
+    avg = total / num_students;
+    return avg;
+}
+/**
+ This function takes an array of scores and the number
+ of students as arguments. It returns the maximum score.
+ */
+int get_highest(int exam_scores[], int num_students) {
+    int max;
+    max = exam_scores[0];
+    for(int i = 0; i < (num_students - 1); i++) {
+        if (max < exam_scores[i+1])
+        max = exam_scores[i+1];
+    }
+    return max;
+}
+/**
+ This function takes an array of scores and the number
+ of students as arguments. It returns the minimum score.
+ */
+int get_lowest(int exam_scores[], int num_students) {
+    int min;
+    min = exam_scores[0];
+    for(int i = 0; i < (num_students - 1); i++) {
+        if (min > exam_scores[i+1])
+            min = exam_scores[i+1];
+    }
+    return min;
+}
+/**
+ This function takes two parallel arrays, maximum score,
+ minimum score, number of students, and average as arguments.
+ It displays the number of students, average score, as well
+ as the student IDs with the highest and lowest score.
+ */
+void display_results(string student_ids[], int exam_scores[], int max,
+                     int min, int num_students, double avg) {
+    cout << "Number of students: " << num_students << endl;
+    cout << "Average score: " << avg << endl;
+    int i = 0;
+    cout << "Student IDs with the highest score of "
+         << max << ": ";
+    for(; i < num_students; i++) {
+        if (max == exam_scores[i])
+            cout << student_ids[i] << " ";
+    }
+    cout << endl;
+    
+    i = 0;
+    cout << "Student IDs with the lowest score of "
+         << min << ": ";
+   for(; i < num_students; i++) {
+        if (min == exam_scores[i])
+            cout << student_ids[i] << " ";
+    }
+    cout << endl;
+}
+/**
+ This function takes an output file, two parallel arrays,
+ the number of students, and the average score as arguments.
+ It writes student ID and the respective score if the score was
+ less than the average.
+ */
+void write_outfile(ofstream &outFile, string student_ids[], int exam_scores[],
+                 int num_students, double avg) {
+    outFile << "Student IDs(and respective scores) with a score\n" 
+            << "lower than the average of " << avg << ":\n";
+    for(int i = 0; i < num_students; i++) {
+        if (avg > exam_scores[i])
+            outFile << student_ids[i] << " " << exam_scores[i] << endl;
+    }
+}
+/**
+ This function displays an end of
+ program message.
+ */
+void farewell() {
+    cout << "This is the end of the program. Thanks for using it.\n";
+}
+
+/****
+Output:
+This program reads scores.txt, which contains student IDs with their
+respective test scores, sorts them into two arrays, and displays:
+-The total number of students
+-The class average
+-lowest score with that student's ID
+-highest score with that student's ID
+The student IDs of those who scored less than the average will
+be stored in an output file with a user-specified name.
+
+Number of students: 13
+Average score: 81.6923
+Student IDs with the highest score of 100: DR123 2ABCD AK323 
+Student IDs with the lowest score of 45: SW111 TY4XZ 9QWE9 
+
+Please enter a file name for the output file: output.txt
+This is the end of the program. Thanks for using it.
+*/
+
 
--- chp7_source/Pr7-20.cpp
+++ chp7_source/Pr7-20.cpp
@@ -1,49 +1,43 @@
 // This program gets a series of test scores and
 // calculates the average of the scores with the
 // lowest score dropped.
-// Find and fix the errors. Run the program once (enter at least 5 scores) and
-// save the output at the end of the source file a a comment
-
-// NAME: Brian Le
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 // Function prototypes
-void getTestScores(double [], int &);
-double getTotal(double [], int &);
-double getLowest(double [], int &);
+void getTestScores(double[], int);
+double getTotal(const double[], int);
+double getLowest(const double[], int);
 
 int main()
 {
-   const int SIZE = 45;      // Array maximum size
-   int size;                // Array actual size
+   const int SIZE = 4;      // Array size
    double testScores[SIZE], // Array of test scores
           total,            // Total of the scores
           lowestScore,      // Lowest test score
           average;          // Average test score
-   
+
+   // Set up numeric output formatting.
+   cout << fixed << showpoint << setprecision(1);
+
    // Get the test scores from the user.
-   getTestScores(testScores, size);
+   getTestScores(testScores, SIZE);
 
    // Get the total of the test scores.
-   total = getTotal(testScores, size);
+   total = getTotal(testScores, SIZE);
 
    // Get the lowest test score.
-   lowestScore = getLowest(testScores, size);
+   lowestScore = getLowest(testScores, SIZE);
 
    // Subtract the lowest score from the total.
    total -= lowestScore;
-   size--; //you mean dividing by size minus 1 right? <===
 
    // Calculate the average. Divide by 3 because
    // the lowest test score was dropped.
-   average = total / size;
-   // Set up numeric output formatting.
-   cout << fixed << showpoint << setprecision(1);
+   average = total / (SIZE - 1);
+
    // Display the average.
-   cout << "The lowest score is " << lowestScore << ".\n";
    cout << "The average with the lowest score "
        << "dropped is " << average << ".\n";
 
@@ -55,16 +49,14 @@ int main()
 // as arguments. It prompts the user to enter test scores,   *
 // which are stored in the array.                            *
 //************************************************************
-void getTestScores(double scores[], int &size)
+
+void getTestScores(double scores[], int size)
 {
    // Loop counter
    int index;
    
-   // Get the number of scores
-   cout << "Enter the number of scores: ";
-   cin >> size;
    // Get each test score.
-   for(index = 0; index < size; index++)
+   for(index = 0; index <= size - 1; index++)
    {
       cout << "Enter test score number " 
            << (index + 1) << ": ";
@@ -78,10 +70,10 @@ void getTestScores(double scores[], int &size)
 // elements is returned as a double.                 *
 //****************************************************
 
-double getTotal(double array[], int &size)
+double getTotal(const double array[], int size)
 {
    double total = 0; // Accumulator
-    
+
    // Add each element to total.
    for (int count = 0; count < size; count++)
       total += array[count];
@@ -96,7 +88,7 @@ double getTotal(double array[], int &size)
 // array is returned as a double.                    *
 //****************************************************
 
-double getLowest(double array[], int &size)
+double getLowest(const double array[], int size)
 {
    double lowest;  // To hold the lowest value
 
@@ -106,7 +98,7 @@ double getLowest(double array[], int &size)
    // Step through the rest of the array. When a
    // value less than lowest is found, assign it
    // to lowest.
-   for (int count = 0; count < size; count++)
+   for (int count = 1; count < size; count++)
    {
       if (array[count] < lowest)
          lowest = array[count];
@@ -115,25 +107,3 @@ double getLowest(double array[], int &size)
    // Return the lowest value.
    return lowest;
 }
-/************ OUTPUT *******************
-Enter the number of scores: 7
-Enter test score number 1: 40
-Enter test score number 2: 46
-Enter test score number 3: 49
-Enter test score number 4: 100
-Enter test score number 5: 86
-Enter test score number 6: 78
-Enter test score number 7: 90
-The lowest score is 40.0.
-The average with the lowest score dropped is 74.8.
----------------------------------------------------
-Enter the number of scores: 5
-Enter test score number 1: 101.9
-Enter test score number 2: 35.6
-Enter test score number 3: 98.6
-Enter test score number 4: 45.9
-Enter test score number 5: 35.9
-The lowest score is 35.6.
-The average with the lowest score dropped is 70.6.
-
- */
